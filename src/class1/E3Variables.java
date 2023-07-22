c36f1e305c5dab27ddc0c7eff35399ccaebe04bb
Asghar <instructor@syntaxtechs.com>
2023-07-22 15:00:49 -0400

    Class1

---
+++ src/class1/E3Variables.java
@@ -0,0 +1,17 @@
+package class1;
+
+public class E3Variables {
+    public static void main(String[] args) {
+        // byte->data type which tells the computer what type of information
+        // we are going to store
+
+        /*
+        age-> variable/container/box/label
+        = ->  this sign is used to store something in a variable/container/box/label
+        16 -> the value that we want to store in age
+         */
+        byte age=16;
+        System.out.println(age);
+
+    }
+}
---
+++ src/class1/E6DataTypes.java
@@ -0,0 +1,19 @@
+package class1;
+
+public class E6DataTypes {
+    public static void main(String[] args) {
+        char gender='M';
+        System.out.println(gender);
+
+        boolean isRaining=false;
+        boolean isHungry=true;
+        boolean wife=true;
+        boolean husband=false;
+
+        char symbol='!';
+
+        String name="Aman";
+        System.out.println(name);
+
+    }
+}
