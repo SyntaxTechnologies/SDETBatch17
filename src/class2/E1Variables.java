22cc2a75ec3f99c1bb2bdf193e3e93f4aff8c3ae
Asghar <instructor@syntaxtechs.com>
2023-07-23 11:01:44 -0400

    Class2

---
+++ src/class2/E1Variables.java
@@ -0,0 +1,11 @@
+package class2;
+
+public class E1Variables {
+    public static void main(String[] args) {
+        int age=80;
+        System.out.println(age);
+        age=81; // updating the value of age variable
+        System.out.println(age);
+
+    }
+}
