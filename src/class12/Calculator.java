4bd99d8eee4cbab456e68790d6711731b3a2aaba
Asghar <instructor@syntaxtechs.com>
2023-08-27 11:00:47 -0400

    class 12 String class

---
+++ src/class12/Calculator.java
@@ -0,0 +1,16 @@
+package class12;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+   void add(){
+       Scanner scanner=new Scanner(System.in);
+       System.out.println("Please Enter the first number");
+       int num1=scanner.nextInt();
+       System.out.println("Please Enter the second number");
+       int num2=scanner.nextInt();
+       int sum=num1+num2;
+       System.out.println("sum of these numbers is "+sum);
+   }
+}
---
+++ src/class12/E2CalculatorTester.java
@@ -0,0 +1,9 @@
+package class12;
+
+public class E2CalculatorTester {
+    public static void main(String[] args) {
+        // we have created the object of the
+        // Calculator class
+        Calculator calc=new Calculator();
+    }
+}
